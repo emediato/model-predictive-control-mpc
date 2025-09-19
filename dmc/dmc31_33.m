585b088092bfd6b4cb005c788a2e8c955efca48a
Mônica Emediato <monicae@ufmg.br>
2025-09-18 23:06:00 -0300

    Update dmc31_33.m

--- dmc/dmc31_33.m
+++ dmc/dmc31_33.m
@@ -11,13 +11,29 @@ close all;
 % ex 3.1 pag 49
 numG = 20;          % Numerador de G
 denG = [1 2 1];    % Denominador de G [s^2, s, termo constante]
-G_tf = tf(numG, denG) + 70; 
+G_tf = tf(numG, denG) + 70.04; 
+
+Nss = 70;
+
+t_final = 14; % Choose appropriate final time
+t = linspace(0, t_final, Nss); % Nss equally spaced time points
+
+
 
 
 figure(1)
-y = step(G_tf) ;
-plot(y)
-title('Resposta ao degrau trocador de calor')
+y = step(G_tf, t);           % Step response with 60 points
+plot(t, y)                   % Plot the response
+grid on
+title('Step Response')
+xlabel('Time (seconds)')
+ylabel('Amplitude')
+
+
+% Verify we have exactly 60 elements
+fprintf('Number of elements in response: %d\n', length(y));
+fprintf('Number of time points: %d\n', length(t));
+
 
 num_coeffs = G_tf.Numerator;
 den_coeffs = G_tf.Denominator;
@@ -42,14 +58,12 @@ init_valor = 70; %degrees
 coefs_g = (y - init_valor ) * 1/delta 
 g=coefs_g';
 
-
-% figure;
-
 figure;
 plot(g);
 title('Resposta ao degrau')
-%% sistema com atraso
 
+
+%% sistema com atraso
 % coeficientes do modelo com atraso
 % Add 6 zeros to the start of the vector 'g'
 g0_padded_start = [zeros(1, 6), g];
@@ -67,24 +81,6 @@ title('Resposta ao degrau trocador de calor com atraso')
 %WRONG
 % coefs_g2 = (g0_padded_start - init_valor ) * 1/delta 
 %g2=coefs_g;
-%%
-len = length(y);
-
-delta_u = repmat([0]',len-1,1);
-h = repmat([0]',len-1,1);
-
-for k = 1:len-1
-    delta_u(k) = y(k+1) - g(k);
-
-    % resposta ao impulso
-    % h(k) = g(k) - g(k-1);
-    %h(k) = g(k) - g(k-1)
-    h(k+1) = g(k+1) - g(k); % delta_g
-
-
-    % resposta do sistema - pagina 47
-    y_new = h(k) * delta_u(k);
-end
 
 
 %% MPC DMC
@@ -96,15 +92,85 @@ Nu = 5 ;
 Nx1 = 1;
 Nx2 = 20;
 
-rbar=1e-1;
-ubar=Nu*rbar % + M*dbar ;
-xbar=Nx*rbar % + Mx*dbar ;
+Nx=Nx2;
 
+rbar=1e-1;
+% ubar=Nu*rbar % + M*dbar ;
+% xbar=Nx*rbar % + Mx*dbar ;
 
 % G tem dimensao 5x20
-G = g';
+a = g';
+
+G=zeros(5,5);
+% i = 1
+G(1:5,1) = a(1:Nu,1);
+% i = 2
+G(1:5,2) = [zeros(1,1)'; a(1:4,1)];
+% i = 3
+G(1:5,3) = [zeros(1,2)'; a(1:3,1)];
+% i = 4
+G(1:5,4) = [zeros(1,3)'; a(1:2,1)];
+
+
+for i = 1:Nu % (N-1) termos subsequentesi=
+    if i==1
+        G(1:Nu,i) = a(1:Nu,1);
+    else
+        aux = Nu-i+1;
+        G(1:Nu,i) = [zeros(1,i-1)'; a(1:aux,1)];
+    end
+end
+
+h = zeros(Nss, Nx);
+
+for i = 1:Nx  
+    for j = 1:Nss
+        if j+ i < Nss
+            h(j,i) = g(j+i) - g(i) ;
+        else
+             h(j,i) = g(Nss) - g(i) ;
+        end
+    end
+end
+
+
+% G_correct = zeros(Nx,Nu);
+% i=1
+% G_correct(1:Nx,i) = a(1:Nx,1)
+% i=2
+% G_correct(1:Nx,i) = [zeros(1,2)'; a(1:Nx-i+1,1)]
+% i=3
+% G_correct(1:Nx,i) = [zeros(1,2)'; a(1:Nx-i+1,1)]
+% i=4
+% G_correct(1:Nx,i) = [zeros(1,3)'; a(1:Nx-i+1,1)]
+
+for i = 1:Nu % (N-1) termos subsequentes
+    if i==1
+        G_correct(1:Nx,i) = a(1:Nx,1);
+    else
+        aux = Nx-i+1;
+        G_correct(1:Nx,i) = [zeros(1,i-1)'; a(1:aux,1)];
+    end
+end
 
 
 
 
- 
+ %%
+len = Nu;
+
+delta_u = repmat([0]',len-1,1);
+h_impulse = repmat([0]',len-1,1);
+
+for k = 1:len-1
+    delta_u(k) = y(k+1) - g(k);
+
+    % resposta ao impulso
+    % h(k) = g(k) - g(k-1);
+    %h(k) = g(k) - g(k-1)
+    h_impulse(k+1) = g(k+1) - g(k); % delta_g
+
+
+    % resposta do sistema - pagina 47
+    y_new = (h_impulse(k) * delta_u(k))/G;
+end
